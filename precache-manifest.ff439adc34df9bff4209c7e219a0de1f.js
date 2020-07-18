be20070ccd8c6441c046763da7de9d65f1c44a98
Henry <soonleqi@gmail.com>
2020-07-18 22:51:25 +0800

    Updates

---
+++ precache-manifest.ff439adc34df9bff4209c7e219a0de1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80a749606250a69f4a9531067ec7e84",
+    "url": "/whitecat-monsters/index.html"
+  },
+  {
+    "revision": "485e41303e5cfbb79cce",
+    "url": "/whitecat-monsters/static/css/main.fda9e1fa.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/whitecat-monsters/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whitecat-monsters/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "485e41303e5cfbb79cce",
+    "url": "/whitecat-monsters/static/js/main.7b2367bb.chunk.js"
+  },
+  {
+    "revision": "56bef0e6cfecfb01fe08",
+    "url": "/whitecat-monsters/static/js/runtime-main.0fcbd248.js"
+  }
+]);
